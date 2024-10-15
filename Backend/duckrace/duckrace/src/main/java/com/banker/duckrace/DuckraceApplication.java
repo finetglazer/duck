77083a9a927f20e155ae2e2d2775a2f9dce87707
I hate iu <tranhung10122003@gmail.com>
2024-10-15 21:26:04 +0700

    feat: init BE project

---
+++ Backend/duckrace/duckrace/src/main/java/com/banker/duckrace/DuckraceApplication.java
@@ -0,0 +1,13 @@
+package com.banker.duckrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuckraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuckraceApplication.class, args);
+	}
+
+}
